2c708a61806c979a2e60abed8141f36e67dae5c0
RejinBK <rejinkayastha@gmail.com>
2021-07-30 17:46:17 +0545

    Small Edits

--- public/js/history.js
+++ public/js/history.js
@@ -3,32 +3,6 @@
 //     console.log(transaction)
 // })
 
-$(function(){
-    var scroll= $(document).scrollTop();
-    var navHeight = $('.navbar').outerHeight();
-
-    $(window).scroll(function(){
-
-        var scrolled= $(document).scrollTop();
-
-        if(scrolled > navHeight){
-            $('.navbar').addClass('animate');
-        }else{
-            $('.navbar').removeClass('animate');
-        }
-
-        if(scrolled > scroll){
-             $('.navbar').removeClass('sticky');
-
-        }else{
-            $('.navbar').addClass('sticky');
-        }
-        
-        scroll = $(document).scrollTop();
-    })
-
-});
-
 
 const displayTransaction = (block) => {
     
